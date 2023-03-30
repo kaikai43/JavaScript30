63e49aaf3d71884f1cb7ef3654c2f9c690d9768f
kaikai43 <jinkai4393@gmail.com>
2023-03-30 12:28:12 +1100

    add fullscreen function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,6 +7,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.full-screen');
 
 // Build functions
 function togglePlay() {
@@ -18,6 +19,35 @@ function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
   toggle.textContent = icon;
 }
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    if (video.requestFullscreen) {
+      video.requestFullscreen().catch(err => {
+        alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+      });
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen().catch(err => {
+        alert(`Error attempting to exit full-screen mode: ${err.message} (${err.name})`);
+      });
+    }
+  }
+  updateFullScreenButton();
+}
+
+function updateFullScreenButton() {
+  fullScreen.innerHTML = document.fullscreenElement ? null :
+  `<svg xmlns="http://www.w3.org/2000/svg" stroke-width="2" stroke="currentColor" style="height: 18px; display: flex;" viewBox="0 0 24 24">
+    <polyline points="22 6 22 2 18 2" stroke-miterlimit="10"/>
+    <polyline points="2 18 2 22 6 22" stroke-miterlimit="10"/>
+    <polyline points="18 22 22 22 22 18" stroke-miterlimit="10"/>
+    <polyline points="6 2 2 2 2 6" stroke-miterlimit="10"/>
+  </svg>`;
+}
+
+
 
 function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
@@ -47,6 +77,8 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+fullScreen.addEventListener('click', () => toggleFullScreen());
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
